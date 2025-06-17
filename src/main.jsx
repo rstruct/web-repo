4875393a1b07a5983fa46d24609833ce16e69523
rstruct <rstruct.engr@gmail.com>
2025-06-18 02:18:22 +0800

    update route

--- src/main.jsx
+++ src/main.jsx
@@ -21,7 +21,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
       <ThemeProvider theme={theme}>
-    <BrowserRouter>
+    <BrowserRouter basename='/web-repo/'>
       <App />
     </BrowserRouter>
     </ThemeProvider>
